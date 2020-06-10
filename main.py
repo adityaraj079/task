8b89fbd275c356e5cc0b3b2ebe0ba13813405ac2
adityaraj079 <55791929+adityaraj079@users.noreply.github.com>
2020-06-10 20:39:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pytesseract as tess
-tess.pytesseract.tesseract_cmd = r'C:\Users\Aditya\AppData\Local\Tesseract-OCR\tesseract.exe'
+tess.pytesseract.tesseract_cmd = r'C:\Users\Aditya\AppData\Local\Tesseract-OCR\tesseract.exe'   #Change the path with your tesseract ocr's path
 from PIL import Image
 
 img = Image.open('text.png')
